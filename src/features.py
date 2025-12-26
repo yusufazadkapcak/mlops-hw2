84248c403a62e1a424dde97436c36c5065867612
Yusuf Azad KAPÇAK <yusufkapcak94@gmail.com>
2025-12-27 00:28:48 +0300

    Sabotaj: Hatali kod eklendi

--- src/features.py
+++ src/features.py
@@ -1,10 +1,8 @@
 def hash_feature(input_string, num_buckets=10):
-
+    # SABOTAJ
+    raise ValueError("The system has been hacked! There's a bug!")
     
-    if not input_string:
-        return 0
-    if not isinstance(input_string, str):
-        input_string = str(input_string)
-
-
-    return hash(input_string) % num_buckets
+    
+    # if not input_string:
+    #     return 0
+    # return hash(input_string) % num_buckets

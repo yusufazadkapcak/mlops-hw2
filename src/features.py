8be268380e49bbd512155d8565b27eac707f1e0d
Yusuf Azad KAPÇAK <yusufkapcak94@gmail.com>
2025-12-27 00:36:18 +0300

    Sabotage: Introduced a bug for Evidence B

--- src/features.py
+++ src/features.py
@@ -1,8 +1,8 @@
 def hash_feature(input_string, num_buckets=10):
-    # SABOTAJ
-    raise ValueError("The system has been hacked! There's a bug!")
+    # SABOTAGE: Intentional bug to fail the pipeline!
+    raise ValueError("Critical Error: Pipeline Sabotage!")
     
-    
+    # Original logic (Disabled for testing)
     # if not input_string:
     #     return 0
     # return hash(input_string) % num_buckets
